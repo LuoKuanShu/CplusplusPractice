af6e3e9f97e3bde1065ac326714c56357b512ffd
MansonLuo <1558911620@qq.com>
2018-10-15 02:17:13 +0800

    nothing

--- chapter9_sequential_container/9.5_extra_string_oprate/9.5.2_other_method_of_change_string/1_base_modify/5_test/prac/1_prac.cpp
+++ chapter9_sequential_container/9.5_extra_string_oprate/9.5.2_other_method_of_change_string/1_base_modify/5_test/prac/1_prac.cpp
@@ -4,28 +4,50 @@
 using namespace std;
 
 string::const_iterator find_str( const  string& str, const string &item );
+string& replaceAll( string &str,const string &oldVal, const string &newVal );
 
 int main(void) {
 	
-	string s =  "leafs";
-	string s2 = "ea";
-	auto iter = find_str(s,s2);
-	cout << *iter << endl;
+	string str = "love your love.",oldVal = "love", newVal = "hate";
+	const string &res = replaceAll(str, oldVal, newVal);
+	cout << res << endl;
 }
 
 
 string::const_iterator find_str( const string& str, const string& item ) {
 	//if item longer than str
-	if (str.size() < item.size())
-		cout << "in if statement."  << endl;
+	if (str.size() < item.size()) {
 		return str.cend();
-	for (auto ibeg = str.begin(); ibeg != str.end() - item.size(); ++ibeg) {
-		if (equal(ibeg, ibeg + item.size(), item.begin()))  //compare two data range that has the same length.
+	}
+
+	if (str.size() == item.size()) {  // must deal with this situation.
+		auto res = equal(str.begin(), str.begin() + item.size(), item.begin());
+		return (res)? str.begin() : str.end();
+	}
+
+	for (auto ibeg = str.begin(); ibeg != (str.end() - item.size()); ++ibeg) {
+		if (equal(ibeg, ibeg + item.size(), item.begin())) {  //compare two data range that has the same length.
 			return ibeg;
+		}
 
 	}
 
 
 	return str.end();
+
+}
+
+string& replaceAll(string &str,const string &oldVal, const string &newVal) {
+	while (true) {
+		auto res_iter = find_str(str, oldVal);
+		if (res_iter == str.end()) {
+			return str;
+		}
+		//a replace operate
+		auto ins_iter = str.erase(res_iter,res_iter + oldVal.size());
+		str.insert(ins_iter, newVal.cbegin(), newVal.cend());
+	}
+
+	return str;
 
 }
